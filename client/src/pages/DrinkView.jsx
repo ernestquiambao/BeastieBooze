1ba86b8768eaa8fc5fa74c82381b33e0029753f2
Zack Izdepski <zizdep1@gmail.com>
2022-02-02 11:23:36 -0600

    Feat: Added a modal component to drinkView

--- client/src/pages/DrinkView.jsx
+++ client/src/pages/DrinkView.jsx
@@ -1,31 +1,33 @@
-import React, {useEffect, useState, useContext} from 'react'
-import { useParams, useLocation } from 'react-router-dom'
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import { Modal, Button } from 'react-bootstrap';
+
 import axios from 'axios';
 
-import { BoozeContext } from '../boozeContext'
-import { UserContext } from '../userContext'
+import { BoozeContext } from '../boozeContext';
+import { UserContext } from '../userContext';
 
-
-import { ingredientParser } from '../../utils/parseIng'
+import { ingredientParser } from '../../utils/parseIng';
+import Review from './Review.jsx';
 
 const DrinkView = () => {
-
   // useParams will grab the param passed in url. grabbing drinkId from params.
-  const { drinkId } = useParams()
-  const [aDrink, setADrink] = useState({})
+  const { drinkId } = useParams();
+  const [aDrink, setADrink] = useState({});
 
-   useEffect(() => {
-    axios.get(`/routes/drink/${drinkId}`)
-    .then(( { data }) => {
-    setADrink(data.drinks[0])
-    })
-    .catch((err) => console.error('THIS IS OUR ERROR!', err, drinkId))
-  }, [])
+  useEffect(() => {
+    axios
+      .get(`/routes/drink/${drinkId}`)
+      .then(({ data }) => {
+        setADrink(data.drinks[0]);
+      })
+      .catch((err) => console.error('THIS IS OUR ERROR!', err, drinkId));
+  }, []);
 
   const ingredients = ingredientParser(aDrink);
 
-  const { isLoggedIn, favoriteDrinks, toggleFavorite, removeFavorite } = useContext(UserContext);
-
+  const { isLoggedIn, favoriteDrinks, toggleFavorite, removeFavorite } =
+    useContext(UserContext);
 
   // grab what we need from drink object, reassign names
   const {
@@ -38,44 +40,61 @@ const DrinkView = () => {
   } = aDrink;
 
   const removeButton = () => {
-    if(favoriteDrinks.includes(name)){
+    if (favoriteDrinks.includes(name)) {
       return (
-        <span className="remove-button" onClick={() => removeFavorite(aDrink)}>
-          <button type="button" className="btn btn-danger" >Remove from Favorites</button>
+        <span className='remove-button' onClick={() => removeFavorite(aDrink)}>
+          <button type='button' className='btn btn-danger'>
+            Remove from Favorites
+          </button>
         </span>
-      )
+      );
     }
-  }
+  };
 
   const userButtons = () => {
-    if(isLoggedIn){
+    if (isLoggedIn) {
       return (
         <>
           <br></br>
-          <span className="drink-button">
-            <button type="button" className="btn btn-dark" onClick={() => { toggleFavorite(aDrink) }}>Add To Favorites</button>
+          <span className='drink-button'>
+            <button
+              type='button'
+              className='btn btn-dark'
+              onClick={() => {
+                toggleFavorite(aDrink);
+              }}
+            >
+              Add To Favorites
+            </button>
+            <div>
+              <Review aDrink={aDrink} />
+            </div>
           </span>
-          { removeButton() }
+          {removeButton()}
         </>
-      )
+      );
     }
-  }
+  };
 
   return (
-    <div className="container">
-      <h2 className="page-heading">{name}</h2>
-      <div className="row">
-        <div className="col-md-8">
-          <img src={thumbnail} className="img-fluid drink-display" alt={name} />
+    <div className='container'>
+      <h2 className='page-heading'>{name}</h2>
+      <div className='row'>
+        <div className='col-md-8'>
+          <img src={thumbnail} className='img-fluid drink-display' alt={name} />
         </div>
-        <div className="col-md-4">
+        <div className='col-md-4'>
           <h4>{alcoholic}</h4>
           <h4>Glass: {glass}</h4>
           <hr></hr>
           <h5>Ingredients</h5>
           <ul>
             {ingredients.map((i, index) => {
-              return <li key={index}>{i[1]}  {i[0]}</li>   //* each element is an array containing an ingredient followed by it's measurement
+              return (
+                <li key={index}>
+                  {i[1]} {i[0]}
+                </li>
+              ); //* each element is an array containing an ingredient followed by it's measurement
             })}
           </ul>
           <h5>Directions</h5>
@@ -86,7 +105,7 @@ const DrinkView = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DrinkView
+export default DrinkView;
