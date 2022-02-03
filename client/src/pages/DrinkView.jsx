44171449d2afba1f967fa778324f6b1e0f4c7687
Zack Izdepski <zizdep1@gmail.com>
2022-02-03 13:47:20 -0600

    added a comment to test pulling changes to VM

--- client/src/pages/DrinkView.jsx
+++ client/src/pages/DrinkView.jsx
@@ -10,6 +10,7 @@ import { UserContext } from '../userContext';
 import { ingredientParser } from '../../utils/parseIng';
 import StarRating from '../components/StarRating.jsx';
 
+// Testing deployed instance workflow //
 const DrinkView = () => {
   // useParams will grab the param passed in url. grabbing drinkId from params.
   const { drinkId } = useParams();
