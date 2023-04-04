71ba30eee1d202d9a01844d2cfcaba7025683235
ernestquiambao <ernest.quiambao@gmail.com>
2023-04-04 17:22:18 -0500

    add mapbox component and mapbox scripts to index.html

---
+++ client/src/components/mapbox.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const mapbox = () => {
+
+    return (
+        <div id='map' style='width: 400px; height: 300px;'></div>
+<script>
+  mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+  });
+</script>
+    
+    )
+}
