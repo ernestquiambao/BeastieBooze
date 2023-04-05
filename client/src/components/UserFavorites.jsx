e8ddbb47168654a0228a74d192b38bdd81f650ab
ernestquiambao <ernest.quiambao@gmail.com>
2023-04-05 11:38:05 -0500

    mapbox component renders on click

--- client/src/components/UserFavorites.jsx
+++ client/src/components/UserFavorites.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserFavorites = ({ favorites }) => {
-
-
   return (
-    <ul className="circle-bullets">
+    <ul className='circle-bullets'>
       {favorites.map((drink, i) => {
-       return drink.idDrink ?
-         (
+        return drink.idDrink ? (
           <div key={i}>
-          <Link to={{
-            pathname: `/drink/${drink.idDrink}`,
-            state: { drink }
-            }}>
-            <li key={i} className="favorite">{drink.strDrink}</li>
-          </Link>
+            <Link
+              to={{
+                pathname: `/drink/${drink.idDrink}`,
+                state: { drink },
+              }}
+            >
+              <li key={i} className='favorite'>
+                {drink.strDrink}
+              </li>
+            </Link>
           </div>
-        )
-        :
-        (
+        ) : (
           <div key={i}>
-          <Link to={{
-            pathname: `/custom/${drink.drinkName || drink.name}`,
-            state: { drink }
-            }}>
-            <li key={i} className="favorite">{drink.drinkName || drink.name}</li>
-          </Link>
+            <Link
+              to={{
+                pathname: `/custom/${drink.drinkName || drink.name}`,
+                state: { drink },
+              }}
+            >
+              <li key={i} className='favorite'>
+                {drink.drinkName || drink.name}
+              </li>
+            </Link>
           </div>
-        )
+        );
       })}
     </ul>
-  )
-}
+  );
+};
 
-export default UserFavorites
+export default UserFavorites;
