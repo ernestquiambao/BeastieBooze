fc49bab7a905a6fc8baff77a4f97ce2134d63f54
teddyrockwell <tmelder@xula.edu>
2023-04-08 18:35:18 -0500

    attempt to fix deployment, no async/await + regen-runtime

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -5,43 +5,44 @@ const { User } = require('../database/Models');
 
 const quizRouter = Router();
 
-quizRouter.get('/user/:googleId', async (req, res) => {
+quizRouter.get('/user/:googleId', (req, res) => {
   const { googleId } = req.params;
 
-  try {
-    const user = await User.findOne({ googleId });
-    // console.log('it works!!')
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
+  User.findOne({ googleId })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send('User not found');
+      }
 
-    res.send(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
+      res.send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
-quizRouter.post('/user/scores', async (req, res) => {
+quizRouter.post('/user/scores', (req, res) => {
   const { googleId, score } = req.body;
 
-  try {
-    const user = await User.findOne({ googleId });
+  User.findOne({ googleId })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    user.scores.push(score);
-    user.scores.sort((a, b) => b - a);
-    user.scores = user.scores.slice(0, 5);
-    await user.save();
-
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
+      user.scores.push(score);
+      user.scores.sort((a, b) => b - a);
+      user.scores = user.scores.slice(0, 5);
+      return user.save();
+    })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    });
 });
 
 
